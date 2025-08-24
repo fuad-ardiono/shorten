ccfac3e7a0d8206cf970ccd88dfe4818b607f369
Muhammad Fuad Ardiono <fuadardiono16@gmail.com>
2025-08-24 22:18:04 +0700

    feat: improve code

--- src/main/java/com/fuad/shorten/modules/link/LinkService.java
+++ src/main/java/com/fuad/shorten/modules/link/LinkService.java
@@ -4,6 +4,6 @@ import com.fuad.shorten.modules.link.dto.request.LinkRequest;
 import com.fuad.shorten.modules.link.dto.response.LinkResponse;
 
 public interface LinkService {
-    public LinkResponse createLink(LinkRequest dto);
-    public String getLink(String shortCode);
+    LinkResponse createLink(LinkRequest dto);
+    String getLink(String shortCode);
 }
